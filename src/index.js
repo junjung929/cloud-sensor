a9a80eac3b671f12e7cf824ab02f330083b7d258
Jun Young, Jung <juny929@gmail.com>
2018-02-08 14:12:11 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,7 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import configureStore from './store'
 import Routes from './routes'
-<<<<<<< HEAD
-import 'styles/global-styles'
-=======
 import './styles/global-styles'
->>>>>>> 3a949bb02b3651485a713c8ba756a2caf926b0e1
 import registerServiceWorker from './utils/registerServiceWorker'
 
 render(
